6174175c242099b366265ca6b38e769ef697a7ce
SamayJain8 <20d100023@iitb.ac.in>
2026-05-06 23:02:49 +0530

    feat: implement packet header

---
+++ core-engine/include/core/packet.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+
+namespace netsim::core {
+
+#pragma pack(push, 1)
+struct PacketHeader {
+    uint8_t version;    // 1 byte
+    uint8_t type;       // 1 byte (0: Data, 1: ACK, 2: Control)
+    uint32_t length;    // 4 bytes (Size of payload)
+};
+#pragma pack(pop)
+
+class Packet {
+public:
+    PacketHeader header;
+    std::vector<uint8_t> payload;
+
+    Packet(uint8_t msg_type, const std::vector<uint8_t>& data);
+
+    // Flattens object to bytes for the socket
+    std::vector<uint8_t> serialize() const;
+    
+    // Rebuilds object from socket bytes
+    static Packet deserialize(const std::vector<uint8_t>& raw_data);
+};
+
+} // namespace netsim::core

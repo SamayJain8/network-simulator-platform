e201a733001fc1d3fc7676f0ce3981dfa793e1dd
SamayJain8 <20d100023@iitb.ac.in>
2026-05-08 23:56:29 +0530

    feat: setup packet header logic

--- core-engine/include/core/packet.hpp
+++ core-engine/include/core/packet.hpp
@@ -7,7 +7,7 @@ namespace netsim::core {
 #pragma pack(push, 1)
 struct PacketHeader {
     uint8_t version;    // 1 byte
-    uint8_t type;       // 1 byte (0: Data, 1: ACK, 2: Control)
+    uint8_t type;       // 1 byte (0: Data, 1: ACK, 2: control)
     uint32_t length;    // 4 bytes (i's the Size of payload)
 };
 #pragma pack(pop)
@@ -22,9 +22,9 @@ public:
 
     // Flattens object to bytes for the socket
     std::vector<uint8_t> serialize() const;
-    
+      
     // Rebuilds object from socket bytes
     static Packet deserialize(const std::vector<uint8_t>& raw_data);
 };
 
-} // namespace netsim::corea
+} // namespace netsim::corea 
